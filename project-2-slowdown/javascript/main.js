b684a05e0f6f7b04db00412fd295154be9c71f37
JoeyCheung-0414 <joeycheung2003@gmail.com>
2025-10-04 22:57:02 -0700

    q
    q

--- project-2-slowdown/javascript/main.js
+++ project-2-slowdown/javascript/main.js
@@ -1,10 +1,30 @@
 console.log("Hello Hello");
 
 
-//Closing Ads---------------------------------------/
+//Closing Ads---------------------------------------//
 
 document.querySelectorAll(".close").forEach(btn => {
     btn.addEventListener("click", () => {
         btn.closest("div[class^='box-']").style.display = "none";
     });
 });
+
+//Pay Error Box 25---------------------------------------//
+document.querySelector(".b25-btn").addEventListener("click", function () {
+    alert("Not Enough Funds...")
+})
+
+//Click Reschedule Box 30------------------------//
+document.querySelector(".btn-reschedule").addEventListener("click", function () {
+    document.querySelector(".box-30-reschedule").style.visibility = "visible";
+    document.querySelector(".box-30").style.visibility = "hidden";
+})
+
+
+
+//Click Error Box 35------------------------//
+
+document.querySelector(".b35-error").addEventListener("click", function () {
+    alert("Oh No, I Don't have the Energy To Work... ")
+})
+
