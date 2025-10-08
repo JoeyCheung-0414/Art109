363a202aaf6527dffcb062a8201fe76749ef288d
JoeyCheung-0414 <joeycheung2003@gmail.com>
2025-10-08 15:33:13 -0700

    project update

--- project-2-slowdown/javascript/main.js
+++ project-2-slowdown/javascript/main.js
@@ -2,29 +2,31 @@ console.log("Hello Hello");
 
 
 //Closing Ads---------------------------------------//
-
 document.querySelectorAll(".close").forEach(btn => {
     btn.addEventListener("click", () => {
         btn.closest("div[class^='box-']").style.display = "none";
     });
 });
 
-//Pay Error Box 25---------------------------------------//
-document.querySelector(".b25-btn").addEventListener("click", function () {
+//Pay Error Box 17---------------------------------------//
+document.querySelector(".b17-text").addEventListener("click", function () {
+    alert("This Call Number is Currently Unavailable...")
+})
+
+//Pay Error Box 18---------------------------------------//
+document.querySelector(".b18-btn").addEventListener("click", function () {
     alert("Not Enough Funds...")
 })
 
-//Click Reschedule Box 30------------------------//
+//Click Reschedule Box 23------------------------//
 document.querySelector(".btn-reschedule").addEventListener("click", function () {
-    document.querySelector(".box-30-reschedule").style.visibility = "visible";
-    document.querySelector(".box-30").style.visibility = "hidden";
+    document.querySelector(".box-23-reschedule").style.visibility = "visible";
+    document.querySelector(".box-23").style.visibility = "hidden";
 })
 
+//Click Error Box 28------------------------//
 
-
-//Click Error Box 35------------------------//
-
-document.querySelector(".b35-error").addEventListener("click", function () {
+document.querySelector(".b28-error").addEventListener("click", function () {
     alert("Oh No, I Don't have the Energy To Work... ")
 })
 
