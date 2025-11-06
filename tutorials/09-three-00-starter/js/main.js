7feeaef7c8a274869b22d680fc5e006868ca878e
JoeyCheung-0414 <joeycheung2003@gmail.com>
2025-11-05 17:22:34 -0800

    3d model

--- tutorials/09-three-00-starter/js/main.js
+++ tutorials/09-three-00-starter/js/main.js
@@ -1,6 +1,6 @@
+
 // Basic Three.JS scene from documentation, importing Three.JS through a CDN 
 // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
-
 
 //~~~~~~~Import Three.js (also linked to as import map in HTML)~~~~~~
 import * as THREE from 'three';
@@ -9,41 +9,90 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+// My Code
+let scene, camera, renderer, cube, capsule;
 
-// ~~~~~~~~~~~~~~~~Set up~~~~~~~~~~~~~~~~
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+function init() {
+    // ~~~~~~~~~~~~~~~~Set up~~~~~~~~~~~~~~~~
+    scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    // Lights-------
+    const light = new THREE.DirectionalLight(0xffffff, 3);
+    light.position.set(1, 1, 1, 5);
+    scene.add(light);
+
+    const helper = new THREE.DirectionalLightHelper(light, 5);
+    scene.add(helper);
+
+    // Light 2-----
+    const lightLeft = new THREE.DirectionalLight(0x00ffff, 3);
+    light.position.set(1, 1, 1, -5);
+    scene.add(lightLeft);
+
+    const helperLeft = new THREE.DirectionalLightHelper(lightLeft, 5);
+    scene.add(helperLeft);
 
 
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-// ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
-const controls = new OrbitControls(camera, renderer.domElement);
-// const loader = new GLTFLoader(); // to load 3d models
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const loader = new GLTFLoader(); // to load 3d models
+
+    loader.load('assets/mc-cat.gltf', function (gltf) {
+        const mcCat = gltf.scene;
+        scene.add(mcCat);
+        mcCat.scale.set(.3, .3, .3);
+
+    })
 
 
+    // ~~~~~~~~~~~~~~~~ Create scene here ~~~~~~~~~~~~~~~~
+    // →→→→→→ Follow next steps in tutorial: 
+    // // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
 
-// ~~~~~~~~~~~~~~~~ Create scene here ~~~~~~~~~~~~~~~~
-// →→→→→→ Follow next steps in tutorial: 
-// // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+    const geometry = new THREE.BoxGeometry(4, 4, 4);
+    
+    const geometry2 = new THREE.CapsuleGeometry( 1, 1, 4, 15,);
 
-const geometry = new THREE.BoxGeometry(3, 3, 3);
-const material = new THREE.MeshBasicMaterial({ color: a3eaff });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    const material2 = new THREE.MeshBasicMaterial({ color: 0xc8ecf7 });
 
-camera.position.z = 5;
+    const texture = new THREE.TextureLoader().load('textures/cat.jpg');
+    const material = new THREE.MeshStandardMaterial({ map: texture });
+
+    capsule = new THREE.Mesh(geometry2, material2);
+    capsule.position.y = 5;
+    scene.add(capsule);
+
+    cube = new THREE.Mesh(geometry, material);
+    cube.position.y = -2;
+    scene.add(cube);
+
+    camera.position.z = 10;
+}
+
 
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 }
 
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
 animate();
