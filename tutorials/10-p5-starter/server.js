55e08c4d485f011dfde27fa943f84b013cc44c42
JoeyCheung-0414 <joeycheung2003@gmail.com>
2025-11-13 14:10:24 -0800

    tut

---
+++ tutorials/10-p5-starter/server.js
@@ -0,0 +1,11 @@
+
+var express = require ('express');
+
+var app = express ();
+var server= app.listen(3000);
+
+app.use(express.static('public'));
+//"static" = host unmoving element
+
+console.log ("Server Running");
+
