1db28b0a3d0112153445f25550d4d6ca26c87a2c
JoeyCheung-0414 <joeycheung2003@gmail.com>
2025-12-07 22:36:22 -0800

    layout update

---
+++ final-project/javascript/main-genre.js
@@ -0,0 +1,43 @@
+
+
+
+console.log("Working... Genre Page");
+
+//Text Box 1  hide + C1 appear on click 
+document.querySelector(".btn-1").addEventListener("click", function () {
+   document.querySelector(".g-text-1").style.visibility = "hidden";
+   document.querySelector(".c1").style.display = "block";
+})
+
+
+
+//Choice 1
+//------------------------------------------------------------------------------------
+
+function dis1 () { 
+    document.querySelector(".c1").style.display = "none";
+}
+
+//Art Hide + c2 text-a appear on click
+document.querySelector(".op1").addEventListener("click", function () {
+    dis1();
+    document.querySelector(".c2-op-1").style.display = "block";
+ })
+
+//Gaming Hide + c2 text-b appear on click
+document.querySelector(".op2").addEventListener("click", function () {
+    dis1();
+    document.querySelector(".c2-op-2").style.display = "block";
+ })
+
+//Makeup Hide + c2 text-c appear on click
+document.querySelector(".op3").addEventListener("click", function () {
+    dis1();
+    document.querySelector(".c2-op-3").style.display = "block";
+ })
+
+//Lifestyle Hide + c2 text-d appear on click
+document.querySelector(".op4").addEventListener("click", function () {
+    dis1();
+    document.querySelector(".c2-op-4").style.display = "block";
+ })
