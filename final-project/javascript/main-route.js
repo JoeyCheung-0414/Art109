bd3dfa1f731c8dc82d57ed81ff0300650d4d7895
JoeyCheung-0414 <joeycheung2003@gmail.com>
2025-12-10 01:12:16 -0800

    img 3

--- final-project/javascript/main-route.js
+++ final-project/javascript/main-route.js
@@ -2,31 +2,31 @@ console.log("Working... Route Page");
 
 //Text Box 1  hide + C1 appear on click 
 document.querySelector(".btn-1").addEventListener("click", function () {
-   document.querySelector(".a-text-1").style.visibility = "hidden";
-   document.querySelector(".c1").style.display = "block";
+  document.querySelector(".a-text-1").style.visibility = "hidden";
+  document.querySelector(".c1").style.display = "block";
 })
 
 
 //Split 
 //------------------------------------------------------------------------------------
 
-function dis1 () { 
-    document.querySelector(".c1").style.display = "none";
+function dis1() {
+  document.querySelector(".c1").style.display = "none";
 }
 
 //C1 Hide + Route-1 appear on click
 document.querySelector(".op1").addEventListener("click", function () {
-    dis1();
-    document.querySelector(".route-1").style.display = "block";
- })
+  dis1();
+  document.querySelector(".route-1").style.display = "block";
+})
 
 //C1 Hide + Route-2 appear on click
 document.querySelector(".op2").addEventListener("click", function () {
-    dis1();
-    document.querySelector(".route-2").style.display = "block";
- })
+  dis1();
+  document.querySelector(".route-2").style.display = "block";
+})
 
- 
+
 // hide class  
 //------------------------------------------------------------------------------------
 document.querySelectorAll(".hide").forEach(btn => {
